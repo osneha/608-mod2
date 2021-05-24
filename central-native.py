d3ab6d80222d60f7657c0f91398191b0fe2a712a
Sneha Ojha <42754345+osneha@users.noreply.github.com>
2021-05-23 21:08:03 -0500

    getting sum, count, mean, median, and mode

---
+++ central-native.py
@@ -0,0 +1,33 @@
+from typing import Counter
+
+
+values = [47, 95, 88, 73, 88, 84]
+
+sort = sorted(values)
+count = (len(values))
+print("Count = ", count)
+
+print("Sum = ",sum(values))
+
+print("Mean = ", sum(values)/len(values) )
+
+if (count % 2==0):
+    median1 = values[count//2]
+    median2 = values[count//2 - 1]
+    median = (median1+median2)/2
+
+else:
+    median = values[count//2]
+
+print("Median = ", median)
+
+data = Counter(values)
+get_mode = dict(data)
+mode = [k for k, v in get_mode.items() if v == max(list(data.values()))]
+
+if len(mode) == count:
+    get_mode = "No mode found"
+else:
+    get_mode = "Mode = " + ', '.join(map(str, mode))
+
+    print(get_mode)
